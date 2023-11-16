074ea0c8885b1f9eb3bc212ad151c68fa20a8b34
Sviluppofwd <sviluppofwd@gmail.com>
2023-11-16 13:41:44 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,25 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Microtex',
+      title: 'Microtext',
       home: Scaffold(
         appBar: AppBar(
           title: Text(_title),
         ),
         body: WebView(
           initialUrl: 'https://www.be-mood.it/app-giacenze.php',
-          onPageStarted: (url) {
-            setState(() {
-              _isLoading = true;
-            });
-          },
-          onPageFinished: (url) {
-            setState(() {
-              _isLoading = false;
-              _title = "Microtex";
-            });
+          javascriptMode: JavascriptMode
+              .unrestricted, // This is needed to enable CSS injection
+          onWebViewCreated: (webView) {
+            // ignore: deprecated_member_use
+            webView.evaluateJavascript('''
+              body {
+                overflow: hidden;
+              }
+            ''');
           },
         ),
       ),
     );
   }
 }
-
-/*
-void main() {
-  runApp(const WebViewPage());
-}
-
-*/
