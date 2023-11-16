562c0df2706f021178402ec18a9a987598170cd9
Sviluppofwd <sviluppofwd@gmail.com>
2023-11-16 17:57:46 +0100

    logo e splashScreen per Android: comunque sarebbero da migliorare se si manda a GooglePlayStore

--- lib/webViewPage.dart
+++ lib/webViewPage.dart
@@ -17,8 +17,9 @@ class _WebViewPage extends State<WebViewPage> {
         title: Text(_title),
       ),
       body: WebView(
-        initialUrl: 'https://www.be-mood.it/app-giacenze.php',
-        javascriptMode: JavascriptMode.unrestricted, // This is needed to enable CSS injection
+        initialUrl: 'https://www.microtexcottonclub.it/app-giacenze.php',
+        javascriptMode: JavascriptMode
+            .unrestricted, // This is needed to enable CSS injection
         onWebViewCreated: (webView) {
           // ignore: deprecated_member_use
           webView.evaluateJavascript('''
